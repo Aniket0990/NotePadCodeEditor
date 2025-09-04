f0b86346a51088bdaa6950412efc4ed5763fb45a
Aniket Shelke <aniketshelke554@gmail.com>
2025-09-04 15:55:36 +0530

    Final Commit

--- CodeEditorDriver.java
+++
@@ -1,9 +0,0 @@
-package project1;
-
-public class CodeEditorDriver {
-
-	public static void main(String[] args) {
-		CodeEditorGUI cd= new CodeEditorGUI();
-	}
-
-}
---
+++ src/CodeEditor/CodeEditorDriver.java
@@ -0,0 +1,9 @@
+package CodeEditor;
+
+public class CodeEditorDriver {
+
+	public static void main(String[] args) {
+		CodeEditorGUI codeEditorGUI = new CodeEditorGUI();
+	}
+
+}
